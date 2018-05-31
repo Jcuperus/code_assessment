44f992e23192bd9b9d5e9399954c669949ca7476
Jaep Cuperus <jaepcuperus@hotmail.com>
2018-05-31 11:52:49 +0000

    PHPMD code unit test

--- feedback_app/tests.py
+++ feedback_app/tests.py
@@ -1,5 +1,14 @@
 from django.test import TestCase
+import code_assessment.settings
+from .utils import AssessmentHelper
 
 # Create your tests here.
-class AssessmentHelperTests(TestCase):
-    def 
+class AssessmentHelperTestCase(TestCase):
+    def test_assess_code_returns_xml(self):
+        file_path = code_assessment.settings.BASE_DIR + '/feedback_app/tmp/test.php'
+        output_type = 'xml'
+        ruleset = 'codesize'
+        output = AssessmentHelper.assess_code([file_path, output_type, ruleset])
+
+        self.assertIsInstance(output, str)
+        self.assertIn('<?xml version="1.0" encoding="UTF-8" ?>', output)
--- feedback_app/utils.py
+++ feedback_app/utils.py
@@ -1,9 +1,19 @@
-from subprocess import run
+import subprocess
+import code_assessment.settings
 
 class AssessmentHelper(object):
     
     @staticmethod
     def assess_code(args):
-        assessment_tool_path = "../vendor/phpmd/phpmd/src/bin/phpmd"
+        assessment_tool_path = code_assessment.settings.BASE_DIR + '/vendor/phpmd/phpmd/src/bin/phpmd'
+        command = [assessment_tool_path] + args
+        command_output = subprocess.run(command, stdout=subprocess.PIPE)
+        return command_output.stdout.decode('utf-8')
 
-        run([assessment_tool_path, args])
+    @staticmethod
+    def test():
+        file_path = code_assessment.settings.BASE_DIR + '/feedback_app/tmp/test.php'
+        output_type = 'xml'
+        rulesets = 'codesize,unusedcode,naming'
+
+        print(AssessmentHelper.assess_code([file_path, output_type, rulesets]))
