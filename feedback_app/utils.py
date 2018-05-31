a311bcb683bb82be20e256cfeaad509c09b7d317
Jaep Cuperus <jaepcuperus@hotmail.com>
2018-05-31 10:11:19 +0200

    Added basic phpmd assessment function

--- feedback_app/utils.py
+++ feedback_app/utils.py
@@ -1,6 +1,9 @@
 from subprocess import run
 
 class AssessmentHelper(object):
+    
     @staticmethod
-    def assess_code():
-        run(["php"])
+    def assess_code(args):
+        assessment_tool_path = "../vendor/phpmd/phpmd/src/bin/phpmd"
+
+        run([assessment_tool_path, args])
