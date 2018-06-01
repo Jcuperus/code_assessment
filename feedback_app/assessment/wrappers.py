5f9047f45457bb4a3adb1e1fa6a219090ee55969
Jaep Cuperus <jaepcuperus@hotmail.com>
2018-06-01 17:11:53 +0200

    Added parse command output method stubs

--- feedback_app/assessment/wrappers.py
+++ feedback_app/assessment/wrappers.py
@@ -1,4 +1,6 @@
 import subprocess
+from xml.etree import ElementTree
+
 import code_assessment.settings
 
 class CliToolWrapper(object):
@@ -7,9 +9,12 @@ class CliToolWrapper(object):
 
     def assess(self, path, rules, output_type):
         output = subprocess.run(self.getAssessmentCommand(path, rules, output_type), stdout=subprocess.PIPE)
-        return output.stdout.decode('utf-8')
+        return self.parseCommandOutput(output.stdout.decode('utf-8'))
 
     def getAssessmentCommand(self, path, rules, output_type):
+        raise NotImplementedError
+
+    def parseCommandOutput(self, output):
         raise NotImplementedError
 
 class PHPCodeSnifferWrapper(CliToolWrapper):
@@ -22,6 +27,13 @@ class PHPCodeSnifferWrapper(CliToolWrapper):
     def getAssessmentCommand(self, path, rules, output_type):
         return [self.cli_path, path, '--standard=' + rules, '--report=' + output_type]
 
+    def parseCommandOutput(self, output):
+        xml = ElementTree.fromstring(output)
+
+        print(xml)
+
+        return output
+
 class PHPMessDetectorWrapper(CliToolWrapper):
     def __init__(self):
         super(PHPMessDetectorWrapper, self).__init__(code_assessment.settings.BASE_DIR + '/vendor/phpmd/phpmd/src/bin/phpmd')
@@ -31,3 +43,6 @@ class PHPMessDetectorWrapper(CliToolWrapper):
 
     def getAssessmentCommand(self, path, rules, output_type):
         return [self.cli_path, path, output_type, rules]
+
+    def parseCommandOutput(self, output):
+        return output
--- feedback_app/tests.py
+++ feedback_app/tests.py
@@ -4,20 +4,30 @@ from .assessment.wrappers import PHPMessDetectorWrapper, PHPCodeSnifferWrapper
 
 # Create your tests here.
 class PHPMessDetectorWrapperTestCase(TestCase):
+    test_file_path = code_assessment.settings.BASE_DIR + '/feedback_app/tmp/test.php'
+
     def test_assess_returns_xml(self):
         phpmd = PHPMessDetectorWrapper()
-        file_path = code_assessment.settings.BASE_DIR + '/feedback_app/tmp/test.php'
-        output = phpmd.assess(file_path)
+        output = phpmd.assess(self.test_file_path)
 
         self.assertIsInstance(output, str)
         self.assertIn('<?xml version="1.0" encoding="UTF-8"', output)
         print(output)
 
+    def test_parse_command_output_returns(self):
+        phpmd = PHPMessDetectorWrapper()
+        raw_output = phpmd.assess(self.test_file_path)
+
+        parsed_output = phpmd.parseCommandOutput(raw_output)
+
+        self.assertNotEqual(raw_output, parsed_output)
+
 class PHPCodeSnifferTestCase(TestCase):
+    test_file_path = code_assessment.settings.BASE_DIR + '/feedback_app/tmp/test.php'
+
     def test_assess_returns_data(self):
         phpcs = PHPCodeSnifferWrapper()
-        file_path = code_assessment.settings.BASE_DIR + '/feedback_app/tmp/test.php'
-        output = phpcs.assess(file_path)
+        output = phpcs.assess(self.test_file_path)
 
         self.assertIsInstance(output, str)
         self.assertIn('<?xml version="1.0" encoding="UTF-8"', output)
