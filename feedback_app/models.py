1361cb84c6b9c2a83c8df125c207ee45611df7dc
Jaep Cuperus <jaepcuperus@hotmail.com>
2018-06-04 11:08:44 +0200

    Added assessment models

--- feedback_app/models.py
+++ feedback_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Assessment(models.Model):
+    date = models.DateTimeField(auto_now=True)
+
+class File(models.Model):
+    assessment = models.ForeignKey(Assessment, on_delete=models.CASCADE)
+    name = models.CharField()
+
+class Error(models.Model):
+    File = models.ForeignKey(Assessment, on_delete=models.CASCADE)
+    line = models.IntegerField()
+    priority = models.IntegerField()
+    category = models.CharField()
+    text = models.TextField()
