01b6d1378a2b756628f5ed8366047a7b3418aac8
Jaep Cuperus <jaepcuperus@hotmail.com>
2018-06-05 15:37:18 +0200

    Possible file write fix

--- feedback_app/assessment/helpers.py
+++ feedback_app/assessment/helpers.py
@@ -1,6 +1,5 @@
 import subprocess
 from django.core.files.storage import default_storage
-from django.core.files import File 
 
 from code_assessment.settings import MEDIA_ROOT
 from .wrappers import PHPCodeSnifferWrapper, PHPMessDetectorWrapper
@@ -8,11 +7,9 @@ from .wrappers import PHPCodeSnifferWrapper, PHPMessDetectorWrapper
 def save_file(file):
     file_path = 'tmp/tmp.php'
 
-    default_storage.save(file_path, file)
-
-    # with File(default_storage.open(file_path)) as destination:
-        # for chunk in file.chunks():
-            # destination.write(chunk)
+    with default_storage.open(file_path, 'w') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
 
     return MEDIA_ROOT + file_path
 
